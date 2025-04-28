7f532f75dcc42b0a9ea2ac81433192d2790ed5df
srivishnu25 <thatisrivishnu@gmail.com>
2025-04-28 19:43:08 +0530

    Add Tailwind CSS integration and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { IconStar } from "./assets/icons";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [star, setStar] = useState(0);
 
   return (
     <>
-      <h1>5 Star Rating</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1 className="mb-8">5 Star Rating</h1>
+      <div className="flex items-center justify-center gap-3">
+        {Array.from({ length: 5 }).map((_, id) => (
+          <button
+            key={id}
+            className={`cursor-pointer bg-transparent w-7 aspect-square text-white ${
+              id + 1 <= star && "!text-orange-300"
+            }`}
+            onClick={() => setStar(id + 1)}
+          >
+            <IconStar />
+          </button>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
